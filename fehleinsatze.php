63bae46e073476460445871b55cc041040b98c6a
semmel1411 <semmel1411@gmx.de>
2022-12-04 19:21:55 +0100

    Create fehleinsatze.php

---
+++ fehleinsatze.php
@@ -0,0 +1,17 @@
+<?php
+
+function writeData($sData, $sFilePath) {
+
+	$sTargetFile = fopen($sFilePath, "a");
+
+	fwrite($sTargetFile, $sData);
+	fclose($sTargetFile);
+
+}
+
+if(isset($_POST['sData'])) {
+
+	writeData($_POST['sData'], "testLog.txt");
+	echo "done";
+
+}
