a4c4e31d206a239b4813faab13b8010d1dc179e0
semmel1411 <semmel1411@gmx.de>
2022-12-05 04:30:40 +0100

    Update fehleinsatze.php

--- fehleinsatze.php
+++ fehleinsatze.php
@@ -3,18 +3,20 @@
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods: GET, POST');
 
-function writeData($sData, $sFilePath) {
-
-	$sTargetFile = fopen($sFilePath, "a");
-
-	fwrite($sTargetFile, $sData);
-	fclose($sTargetFile);
-
+var whitelist = ['http://leitstellenspiel.de']
+var corsOptionsDelegate = function (req, callback) {
+    var corsOptions;
+    if (whitelist.indexOf(req.header('Origin')) !== -1) {
+        corsOptions = {origin: true} // reflect (enable) the requested origin in the CORS response
+    } else {
+        corsOptions = {origin: false} // disable CORS for this request
+    }
+    corsOptions.methods= "GET,HEAD,PUT,PATCH,POST,DELETE"; 
+    callback(null, corsOptions) // callback expects two parameters: error and options
 }
 
-if(isset($_POST['sData'])) {
 
-	writeData($_POST['sData'], "testLog.txt");
-	echo "done";
-
-}
+router.post('/score', cors(corsOptionsDelegate), function (req, res, next) {
+    
+    res.status(200).send('Ok');
+});
