1723b9aada0db5ce999b578bcb6150b8659f7266
semmel1411 <semmel1411@gmx.de>
2022-12-05 04:12:36 +0100

    Update fehleinsatze.php

--- fehleinsatze.php
+++ fehleinsatze.php
@@ -1,6 +1,6 @@
 <?php
 
-header("Access-Control-Allow-Origin: https://www.leitstellenspiel.de");
+header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods: GET, POST');
 
 function writeData($sData, $sFilePath) {
